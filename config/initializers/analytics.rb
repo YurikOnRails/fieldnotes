8f1813d4d96a0d7c4ce8607fd18ceb160bc172f7
YurikOnRails <andrei.iurik@gmail.com>
2026-03-08 16:20:44 +0300

    f: 9

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,3 +1,7 @@
 Rails.application.config.after_initialize do
-  Rails.event.subscribe(AnalyticsSubscriber.new)
+  TRACKED_EVENTS = %w[essay.viewed field.viewed].freeze
+
+  Rails.event.subscribe(AnalyticsSubscriber.new) do |event|
+    TRACKED_EVENTS.include?(event[:name])
+  end
 end
--- test/subscribers/analytics_subscriber_test.rb
+++ test/subscribers/analytics_subscriber_test.rb
@@ -6,4 +6,16 @@ class AnalyticsSubscriberTest < ActiveSupport::TestCase
       Rails.event.notify("essay.viewed", essay_id: 1, path: "/essays/test")
     end
   end
+
+  test "creates page_view on field.viewed event" do
+    assert_difference("PageView.count", 1) do
+      Rails.event.notify("field.viewed", series_id: 1, path: "/field/iceland-2026")
+    end
+  end
+
+  test "ignores untracked events" do
+    assert_no_difference("PageView.count") do
+      Rails.event.notify("some.other.event", foo: "bar")
+    end
+  end
 end
